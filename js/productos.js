9fb82a64989c2249665e1f0ebff6bbd65ed3ea54
limkimpark2 <omartaboada1990@gmail.com>
2022-10-31 18:02:12 -0400

    terminado

--- js/productos.js
+++ js/productos.js
@@ -90,6 +90,96 @@ function cargarDatos() {
             categoria: "Categoria #1",
             imagen: "./productos/10.jpg",
         },
+        {
+            id: 11,
+            nombre: "Ryzen 9 7950x",
+            descripcion:
+                "Procesador AMD Ryzen 9 7950X - 16-Core 4.5 GHz - Socket AM5",
+            precio: 5570,
+            categoria: "Categoria #1",
+            imagen: "./productos/11.jpg",
+        },
+        {
+            id: 12,
+            nombre: "Ryzen 7 7700x",
+            descripcion:
+                "Procesador AMD Ryzen 7 7700X - 8-Core 4.5 GHz - Socket AM5",
+            precio: 3480,
+            categoria: "Categoria #1",
+            imagen: "./productos/12.jpg",
+        },
+        {
+            id: 13,
+            nombre: "Ryzen 5 7600x",
+            descripcion:
+                "Procesador AMD Ryzen 5 7600X - 6-Core 4.7 GHz - Socket AM5",
+            precio: 2720,
+            categoria: "Categoria #1",
+            imagen: "./productos/13.jpg",
+        },
+        {
+            id: 14,
+            nombre: "Asus Rog x670e Crosshair Extreme",
+            descripcion:
+                "Trayendo un legado de grandeza ROG junto con una ráfaga de la última tecnología para ensambles completamente cargados ",
+            precio: 6500,
+            categoria: "Categoria #1",
+            imagen: "./productos/14.jpg",
+        },
+        {
+            id: 15,
+            nombre: "Asus Rog x670e Crosshair Hero",
+            descripcion:
+                "Tarjeta madre AMD X670 ATX con 18 + 2 fases de poder, compatibilidad con DDR5, cinco puertos M.2, puerto de panel frontal",
+            precio: 5500,
+            categoria: "Categoria #1",
+            imagen: "./productos/15.jpg",
+        },
+        {
+            id: 16,
+            nombre: "Asus Prime x670",
+            descripcion:
+                "Placa base ATX para AMD X670 Ryzen™ AM5 con soporte de almacenamiento PCIe 5.0, tres ranuras M.2, DDR5, USB 3.2 Gen 2x2 de tipo C®, compatible con USB4® y Ethernet de 2,5 Gb",
+            precio: 3000,
+            categoria: "Categoria #1",
+            imagen: "./productos/16.jpg",
+        },
+        {
+            id: 17,
+            nombre: "Corsair Dominator DDR5 5200MHz",
+            descripcion:
+                "Kit de memoria DDR5 DRAM DOMINATOR PLATINUM RGB de 32 GB (2x16 GB) a 5200 MHz C38 — Negro",
+            precio: 1500,
+            categoria: "Categoria #1",
+            imagen: "./productos/17.jpg",
+        },
+        {
+            id: 18,
+            nombre: "Adata XPG DDR5 5200MHz",
+            descripcion:
+                "El XPG LANCER DDR5 cuenta con un IC de administración de energía (PMIC) integrado que mejora la estabilidad de la fuente de alimentación. Su voltaje de funcionamiento más bajo también hace que LANCER ",
+            precio: 1400,
+            categoria: "Categoria #1",
+            imagen: "./productos/18.jpg",
+        },
+        {
+            id: 19,
+            nombre: "SSD M.2 Samsung Evo 970",
+            descripcion:
+                "El SSD 970 EVO te ofrece unas velocidades de lectura y escritura sorprendentemente rápidas, una fiabilidad líder en el mercado, así como distintas capacidades de hasta 2 TB*. La última tecnología V-NAND",
+            precio: 1100,
+            categoria: "Categoria #1",
+            imagen: "./productos/19.jpg",
+        },
+        {
+            id: 20,
+            nombre: "Fuente Aorus 1200w Platinum",
+            descripcion:
+                "Digital LCD monitor 80 PLUS Platinum certified Fully modular design 100% Japanese capacitors Compact size design 140mm smart double ball bearing Fan dust removal function Single +12V rail",
+            precio: 4000,
+            categoria: "Categoria #1",
+            imagen: "./productos/20.jpg",
+        },
     ];
 
     localStorage.setItem("productos", JSON.stringify(productosJson));
